b32891c3dbe3979737dc7850ce74092f2a749f8c
Domaman202 <vip.domaman@mail.ru>
2020-12-15 18:42:33 +0300

    Update DataImpl.kt

--- src/commonMain/kotlin/ru.DmN.DmNProject/Data/DataImpl.kt
+++ src/commonMain/kotlin/ru.DmN.DmNProject/Data/DataImpl.kt
@@ -23,65 +23,82 @@ interface IDmNPNumber
     //
     companion object {
         fun of(value: Any?): IDmNPNumber? {
-            var n: IDmNPNumber? = null
+            return when (value) {
+                is Double   -> ofDouble(value)
+                is Float    -> ofFloat(value)
+                is Long     -> ofLong(value)
+                is Int      -> ofInt(value)
+                is Short    -> ofShort(value)
+                is Byte     -> ofByte(value)
+                else        -> DmNPNNull()
+            }
+        }
 
-            if (value is Double) {
-                if (value - value.roundToLong() == 0.0) {
-                    n = if (value >= Byte.MIN_VALUE && value <= Byte.MAX_VALUE)
-                        DmNPNByte(value.toInt().toByte())
-                    else if (value >= Short.MIN_VALUE && value <= Short.MAX_VALUE)
-                        DmNPNShort(value.toInt().toShort())
-                    else if (value >= Int.MIN_VALUE && value <= Int.MAX_VALUE)
-                        DmNPNInt(value.toInt())
-                    else if (value >= Float.MIN_VALUE && value <= Float.MAX_VALUE)
-                        DmNPNFloat(value.toFloat())
-                    else if (value >= Long.MIN_VALUE && value <= Long.MAX_VALUE)
-                        DmNPNLong(value.toLong())
-                    else
-                        DmNPNDouble(value)
-                } else {
-                    n = if (value >= Float.MIN_VALUE && value <= Float.MAX_VALUE)
-                        DmNPNFloat(value.toFloat())
-                    else
-                        DmNPNDouble(value)
-                }
-            } else if (value is Float) {
-                n = if (value - value.roundToLong() == 0.0F) {
-                    if (value >= Byte.MIN_VALUE && value <= Byte.MAX_VALUE)
-                        DmNPNByte(value.toInt().toByte())
-                    else if (value >= Short.MIN_VALUE && value <= Short.MAX_VALUE)
-                        DmNPNShort(value.toInt().toShort())
-                    else if (value >= Int.MIN_VALUE && value <= Int.MAX_VALUE)
-                        DmNPNInt(value.toInt())
-                    else
-                        DmNPNFloat(value)
-                } else
-                    DmNPNFloat(value)
-            } else if (value is Long) {
-                n = if (value >= Byte.MIN_VALUE && value <= Byte.MAX_VALUE)
-                    DmNPNByte(value.toByte())
+        inline fun ofDouble(value: Double): IDmNPNumber {
+            return if (value - value.roundToLong() == 0.0) {
+                if (value >= Byte.MIN_VALUE && value <= Byte.MAX_VALUE)
+                    DmNPNByte(value.toInt().toByte())
                 else if (value >= Short.MIN_VALUE && value <= Short.MAX_VALUE)
-                    DmNPNShort(value.toShort())
+                    DmNPNShort(value.toInt().toShort())
+                else if (value >= Int.MIN_VALUE && value <= Int.MAX_VALUE)
+                    DmNPNInt(value.toInt())
+                else if (value >= Float.MIN_VALUE && value <= Float.MAX_VALUE)
+                    DmNPNFloat(value.toFloat())
+                else if (value >= Long.MIN_VALUE && value <= Long.MAX_VALUE)
+                    DmNPNLong(value.toLong())
+                else
+                    DmNPNDouble(value)
+            } else {
+                if (value >= Float.MIN_VALUE && value <= Float.MAX_VALUE)
+                    DmNPNFloat(value.toFloat())
+                else
+                    DmNPNDouble(value)
+            }
+        }
+
+        inline fun ofFloat(value: Float): IDmNPNumber {
+            return if (value - value.roundToLong() == 0.0F) {
+                if (value >= Byte.MIN_VALUE && value <= Byte.MAX_VALUE)
+                    DmNPNByte(value.toInt().toByte())
+                else if (value >= Short.MIN_VALUE && value <= Short.MAX_VALUE)
+                    DmNPNShort(value.toInt().toShort())
                 else if (value >= Int.MIN_VALUE && value <= Int.MAX_VALUE)
                     DmNPNInt(value.toInt())
                 else
-                    DmNPNLong(value)
-            } else if (value is Int) {
-                n = if (value >= Byte.MIN_VALUE && value <= Byte.MAX_VALUE)
-                    DmNPNByte(value.toByte())
-                else if (value >= Short.MIN_VALUE && value <= Short.MAX_VALUE)
-                    DmNPNShort(value.toShort())
-                else
-                    DmNPNInt(value)
-            } else if (value is Short) {
-                n = if (value >= Byte.MIN_VALUE && value <= Byte.MAX_VALUE)
-                    DmNPNByte(value.toByte())
-                else
-                    DmNPNShort(value)
-            } else if (value is Byte)
-                n = DmNPNByte(value)
+                    DmNPNFloat(value)
+            } else
+                DmNPNFloat(value)
+        }
 
-            return n
+        inline fun ofLong(value: Long): IDmNPNumber {
+            return if (value >= Byte.MIN_VALUE && value <= Byte.MAX_VALUE)
+                DmNPNByte(value.toByte())
+            else if (value >= Short.MIN_VALUE && value <= Short.MAX_VALUE)
+                DmNPNShort(value.toShort())
+            else if (value >= Int.MIN_VALUE && value <= Int.MAX_VALUE)
+                DmNPNInt(value.toInt())
+            else
+                DmNPNLong(value)
+        }
+
+        inline fun ofInt(value: Int): IDmNPNumber {
+            return if (value >= Byte.MIN_VALUE && value <= Byte.MAX_VALUE)
+                DmNPNByte(value.toByte())
+            else if (value >= Short.MIN_VALUE && value <= Short.MAX_VALUE)
+                DmNPNShort(value.toShort())
+            else
+                DmNPNInt(value)
+        }
+
+        inline fun ofShort(value: Short): IDmNPNumber {
+            return if (value >= Byte.MIN_VALUE && value <= Byte.MAX_VALUE)
+                DmNPNByte(value.toByte())
+            else
+                DmNPNShort(value)
+        }
+
+        inline fun ofByte(value: Byte): IDmNPNumber {
+            return DmNPNByte(value)
         }
 
         fun ofType(value: Any?): IDmNPNumber? {
